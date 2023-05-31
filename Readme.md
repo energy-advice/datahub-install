187fb599c44b530b2c6b074cb7ae9c1c50994c77
Šarunas <sarunas.stasaitis@energyadvice.lt>
2023-05-31 15:50:03 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,37 @@
+ Visi aprašyti žingsniai yra įgyvendinti scripte `datacollector_install.sh`
+ 
+## Datahub diegimas
+
+1. Įdiegti docker
+
+Oficiali instrukcija: https://docs.docker.com/engine/install/ubuntu/  
+
+2. Leisti administruojančiam vartotojui valdyti docker (neprivaloma):
+
+Oficiali instrukcija: https://docs.docker.com/engine/install/linux-postinstall/
+
+3. Parsisiųsti `datahub` `docker-compose` šabloną į instaliacijos aplanką (įprastai `/opt/datahub`):
+
+
+    cd /opt
+    mkdir datahub
+    cd datahub
+    git clone https://github.com/energy-advice/datacollector-template.git ./ 
+
+4. Užpildyti reikalingus laukus konfigūracijos failuose:
+
+`datacollector.properties`:
+* `---SERVICE_ID_NOT_SET---` pakeisti į pasirinką, dažniausiai `datacollector`.
+* `---TOKEN_NOT_SET---` pakeisti į EASAS vartotojo, kurio vardu bus keliami duomenys, prisijungimo tokeną
+kurį galima gauti EASAS vartotojų valdymo lange, ant vartotojo paspaudus dešiniu pelės klavišu ir pasirinkus
+"JWT generation".
+Būtina pasirinkti teisę `DATASET_DATA_PUSH` ir nustatyti atitinkamą `TTL` (time to live).
+* Jei kompanija nėra pirmame EASAS serveryje pakeisti `lt.energyadvice.datacollector.remoteServiceUrl` vertę
+į atitinkamą
+
+`.env`:
+* `---DATAHUB_TOKENAS---` pakeisti į datahub tokeną, kuris gaunamas EASAS Datahubs skiltyje paspaudus `Generate`
+* mygtuką
+
+5. Paleisti `datahub` su `docker compose up -d`
+6. `datahub` turėtų atsirasti EASAS Datahubs sąraše, kur galima atlikti skaitymo konfigūraciją
---
+++ datacollector_install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#nutraukia scriptą betkuriai komandai pasibaigus nenuliniu rezultatu
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root (sudo)." >&2;
+    exit 1;
+fi
+
+#surinkti reikalingus duomenis 
+DEFAULT_SERVICE_ID="datacollector"
+
+echo "Enter service ID for datacollector ($DEFAULT_SERVICE_ID):"
+read -rp SERVICE_ID
+
+echo "Enter datacollector push token:"
+read -rp PUSH_TOKEN
+if [[ -z "$PUSH_TOKEN" ]]; then
+    echo "Push token is required"
+    exit 1
+fi
+
+echo "Enter datahub token:"
+read -rp DATAHUB_TOKEN
+if [[ -z "$DATAHUB_TOKEN" ]]; then
+    echo "Datahub token not set"
+fi
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+#Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+apt update
+apt install ca-certificates curl gnupg -y
+
+#Add Docker’s official GPG key:
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#Use the following command to set up the repository:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+
+
+#Install Docker Engine, containerd, and Docker Compose.
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+if ! docker run --rm hello-world ; then
+    exit 1
+fi
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+#create datahub dir
+cd /opt
+mkdir datahub
+cd datahub
+
+
+git clone https://github.com/energy-advice/datacollector-template.git ./
+
+# datacollector.properties ---SERVICE_ID_NOT_SET---
+# datacollector.properties ---TOKEN_NOT_SET---
+# .env ---DATAHUB_TOKENAS---
+
+sed -i "s/---SERVICE_ID_NOT_SET---/${SERVICE_ID:-$DEFAULT_SERVICE_ID}/" datacollector.properties
+sed -i "s/---TOKEN_NOT_SET---/${PUSH_TOKEN}/" datacollector.properties
+if [[ -n "$DATAHUB_TOKEN" ]]; then
+    sed -i "s/---DATAHUB_TOKENAS---/${DATAHUB_TOKEN}/" .env
+fi
+
+
+REALUSER="${SUDO_USER:-$USER}"
+chown "$REALUSER:$REALUSER" ./*
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+docker compose pull
+
+read -rp "Setup docker for access without sudo? (y/N) " choice
+choice="${choice:-N}"  # Set default value to "Y" if empty
+
+if [[ $choice =~ ^[Yy]$ ]]; then
+    groupadd docker
+    usermod -aG docker $USER
+    newgrp docker
+fi
+
+
+read -rp "Installation complete, start datahub? (Y/n) " choice
+choice="${choice:-Y}"  # Set default value to "Y" if empty
+
+if [[ $choice =~ ^[Yy]$ ]]; then
+    docker compose up -d
+fi
+
